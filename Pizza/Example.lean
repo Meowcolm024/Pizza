48d193ac7709cab75a09792ca0a3f3c5c1642ebc
Malcolm Law <23007687+Meowcolm024@users.noreply.github.com>
2025-11-15 11:09:58 +0800

    added mixfix

--- Pizza/Example.lean
+++ Pizza/Example.lean
@@ -10,24 +10,28 @@ def perens (op : Char) : Option (Char → Bool) :=
   | '(' => some (λ r => r == ')')
   | _   => none
 
-def infixOp (op : Char) : Option (Nat × Nat) :=
+def infixOp (op : Char) : Option (Nat × Nat × Option (Char → Bool)) :=
   match op with
-  | '+' | '-' => some (1, 2)
-  | '*' | '/' => some (3, 4)
-  | '.'       => some (10, 9)
+  | '='       => some (3, 2, none)
+  | '?'       => some (5, 4, some λ c => c == ':')
+  | '+' | '-' => some (6, 7, none)
+  | '*' | '/' => some (8, 9, none)
+  | '.'       => some (15, 14, none)
   | _         => none
 
-def prefixOp (op : Char) : Option Nat :=
+def prefixOp (op : Char) : Option (Nat × Option (Char → Bool)) :=
   match op with
-  | '+' | '-' => some 5
+  | 'λ'       => some (2, some λ c => c == '→')
+  | '+' | '-' => some (10, none)
   | _         => none
 
-def postfixOp (op : Char) : Option Nat :=
+def postfixOp (op : Char) : Option (Nat × Option (Char → Bool)) :=
   match op with
-  | '!' => some 7
+  | '!' => some (12 , none)
+  | '[' => some (12 , some λ c => c == ']')
   | _   => none
 
-def mathLang : Language Char :=
+def exampleLang : Language Char :=
   .mk perens prefixOp infixOp postfixOp
 
 def tokenize (s : String) : Array (Token Char) :=
@@ -42,7 +46,8 @@ def tokenize (s : String) : Array (Token Char) :=
 def parse (s : String) : ParseResult (Token Char) (SExp Char × List (Token Char)) :=
   let tokens := tokenize s
   let lexer := Lexer.mk (.mk (n := tokens.size) tokens (by omega))
-  (mathLang.mkParser.run lexer).map (λ r => (r.value, r.rest.tokens.toList))
+  (exampleLang.mkParser.run lexer).map (λ r => (r.value, r.rest.tokens.toList))
 
 #eval parse "- 1 * (2! + 3) - 4.5"
 #eval parse "((1))"
+#eval parse "(λ 2 = 3 → 1 + (2 ? 3[4.5] : 6!)) * (4 + 5)"
--- Pizza/Language.lean
+++ Pizza/Language.lean
@@ -31,17 +31,21 @@ instance {τ} [ToString τ] : ToString (SExp τ) where
 
 structure Language (τ : Type) where
   mk        ::
+  -- parens: closing paren
   parens    : τ → Option (τ → Bool)
-  prefixOp  : τ → Option Nat
-  infixeOp  : τ → Option (Nat × Nat)
-  postfixOp : τ → Option Nat
+  -- prefix: rbp, optionally closing mixfix operator
+  prefixOp  : τ → Option (Nat × Option (τ → Bool))
+  -- infix: lbp, rbp, ...
+  infixeOp  : τ → Option (Nat × Nat × Option (τ → Bool))
+  --- postfix: lbp, ...
+  postfixOp : τ → Option (Nat × Option (τ → Bool))
 
 private def asParseResult {α τ} (opt : Option α) (msg : String) (rem : List τ) : ParseResult τ α :=
   match opt with
   | .some x => .ok x
   | .none   => .error (.mk msg rem)
 
-private def matchParen (f : τ → Bool) (t : Token τ) : Bool :=
+private def matchClosing (f : τ → Bool) (t : Token τ) : Bool :=
   match t with
   | .op p => f p
   | _     => false
@@ -58,15 +62,30 @@ def Language.mkParser {τ} [ToString τ] (lang : Language τ) : ⟦ Parser (Toke
       let p := rec.call (Nat.le_trans small ev)
       match value with
       | .op op =>
-        if let some lbp := lang.postfixOp op then
+        if let some (lbp, cl) := lang.postfixOp op then
           if lbp >= mbp then
-            let res := (.cons op [lhs])
+            if let some cl := cl then
+              let Success.mk rhs small lexer <- p.parse (by omega) lexer 0
+              let Success.mk rop small lexer <- lexer.nextToken
+              if matchClosing cl rop then
+                let res := .cons op [lhs, rhs]
+                let auxres <- p.parseAux (by omega) lexer res mbp
+                return return auxres aux .mk res (by omega) lexer
+            let res := .cons op [lhs]
             let auxres <- p.parseAux (by omega) lexer res mbp
             return return auxres aux .mk res (by omega) lexer
-        if let some (lbp, rbp) := lang.infixeOp op then
+        if let some (lbp, rbp, cl) := lang.infixeOp op then
           if lbp < mbp then return none
           else
-            let (Success.mk rhs small lexer) <- p.parse (by omega) lexer rbp
+            if let some cl := cl then
+              let Success.mk mhs small lexer <- p.parse (by omega) lexer rbp
+              let Success.mk rop small lexer <- lexer.nextToken
+              if matchClosing cl rop then
+                let Success.mk rhs small lexer <- p.parse (by omega) lexer rbp
+                let res := SExp.cons op [lhs, mhs, rhs]
+                let auxres <- p.parseAux (by omega) lexer res mbp
+                return return auxres aux .mk res (by omega) lexer
+            let Success.mk rhs small lexer <- p.parse (by omega) lexer rbp
             let res := SExp.cons op [lhs, rhs]
             let auxres <- p.parseAux (by omega) lexer res mbp
             return return auxres aux .mk res (by omega) lexer
@@ -77,21 +96,29 @@ def Language.mkParser {τ} [ToString τ] (lang : Language τ) : ⟦ Parser (Toke
     return none
   )
   (λ ev lexer mbp => do
-    let (Success.mk value small lexer) <- lexer.nextToken
+    let Success.mk value small lexer <- lexer.nextToken
     let p := rec.call (Nat.le_trans small ev)
     match value with
     | .atom a =>
       let auxres <- p.parseAux (by omega) lexer (.atom a) mbp
       return auxres aux Success.mk (.atom a) (by omega) lexer
     | .op op =>
-      if let .some matched := lang.parens op then
-        let (Success.mk lhs small lexer) <- p.parse (by omega) lexer 0
-        let (Success.mk rpr small lexer) <- lexer.nextToken
-        if matchParen matched rpr then
+      if let .some cl := lang.parens op then
+        let Success.mk lhs small lexer <- p.parse (by omega) lexer 0
+        let Success.mk rpr small lexer <- lexer.nextToken
+        if matchClosing cl rpr then
           let auxres <- p.parseAux (by omega) lexer lhs mbp
           return auxres aux .mk lhs (by omega) lexer
-      if let .some rbp := lang.prefixOp op then
-        let (Success.mk lhs small lexer) <- p.parse (by omega) lexer rbp
+      if let .some (rbp, cl) := lang.prefixOp op then
+        if let some cl := cl then
+          let Success.mk lhs small lexer <- p.parse (by omega) lexer 0
+          let Success.mk rop small lexer <- lexer.nextToken
+          if matchClosing cl rop then
+            let Success.mk rhs small lexer <- p.parse (by omega) lexer rbp
+              let res := SExp.cons op [lhs, rhs]
+              let auxres <- p.parseAux (by omega) lexer res mbp
+              return auxres aux .mk res (by omega) lexer
+        let Success.mk lhs small lexer <- p.parse (by omega) lexer rbp
         let res := .cons op [lhs]
         let auxres <- p.parseAux (by omega) lexer res mbp
         return auxres aux .mk res (by omega) lexer
